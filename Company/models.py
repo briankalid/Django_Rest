45f34e5b2464c613b4adba904150e895839f8e61
Brian Kalid Garcia Olivo <58197698+briankalid@users.noreply.github.com>
2021-07-18 22:48:23 -0500

    Update models.py

--- Company/models.py
+++ Company/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class company(models.Model):
     name = models.CharField(max_length=100,unique=True)
     status = models.CharField(max_length=10, default='activa')
-    id =  models.CharField(max_length=33,primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id =  models.CharField(max_length=100,primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     # id =  models.CharField(max_length=32,primary_key=True, default=str(uuid.uuid4), editable=False)
 
     def __str__(self):
